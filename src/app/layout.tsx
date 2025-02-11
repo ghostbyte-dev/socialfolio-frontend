9782cbc42785d046c942965898b84ea94d92b1b5
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-02-11 23:36:45 +0100

    move components directory out of app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
