08e22cffe5e08311c88d83722871310153ea2523
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-02-18 15:32:21 +0100

    small design fixes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ children, link }) => {
   return (
     <a href={link}>
-      <button className="relative px-4 py-2 text-black bg-amber-400 font-semibold rounded-full border-none overflow-hidden group">
+      <button className="relative px-4 py-2 text-black bg-primary font-semibold rounded-full border-none overflow-hidden group">
         <span className="relative z-10 group-hover:text-white transition-colors duration-300 ease-in-out">
           {children}
         </span>
