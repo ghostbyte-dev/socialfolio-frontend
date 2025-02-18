8589ef0513c3d554211e7f5b67667a2b2306df18
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-02-18 15:31:59 +0100

    Start creating landingpage

--- src/app/(all)/page.tsx
+++ src/app/(all)/page.tsx
@@ -1,9 +1,20 @@
+import Button from "@/components/Button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      fief
+    <div className="items-center flex flex-col">
+      <section className="min-h-[70vh] flex flex-col items-center">
+        <h1 className="text-6xl font-bold mb-6">
+          Your Fediverse Portfolio, Simplified.
+        </h1>
+        <h2 className="text-3xl mb-6">
+          Create a beautiful portfolio to share your projects, links, and social
+          media. Open-source, privacy-friendly, and fully customizable.
+        </h2>
+
+        <Button link="/auth/login">Get Started</Button>
+      </section>
     </div>
   );
 }
