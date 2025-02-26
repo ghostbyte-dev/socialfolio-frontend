e04945ba1bbfc36f32e5b75e6b99d68ff9e1fcff
daniebeler <hiebeler.daniel@gmail.com>
2025-02-26 12:53:08 +0100

    fix build error svgr webpack

--- next.config.ts
+++ next.config.ts
@@ -22,15 +22,13 @@ const nextConfig: NextConfig = {
     ],
     domains: ["*"],
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
   },
 };
 
