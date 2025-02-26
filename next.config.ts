3ebad3b608e55220420eda9a5262c3237f495989
daniebeler <hiebeler.daniel@gmail.com>
2025-02-26 13:07:16 +0100

    try svgr turbo

--- next.config.ts
+++ next.config.ts
@@ -22,13 +22,15 @@ const nextConfig: NextConfig = {
     ],
     domains: ["*"],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
   },
 };
 
