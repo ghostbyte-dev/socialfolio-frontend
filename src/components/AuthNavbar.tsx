fba303213bbd28dc9f6ff552036396abe3208e9b
daniebeler <hiebeler.daniel@gmail.com>
2025-02-27 12:47:00 +0100

    add auth navbar

---
+++ src/components/AuthNavbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { ThemeSwitcher } from "./ThemeSwitcher";
+
+export default function AuthNavbar() {
+  return (
+    <nav className="py-4 px-8 flex justify-between items-center relative">
+        <div className="hidden md:block">
+        <Link href="/explore">
+          <span className="text-lg">Explore</span>
+        </Link>
+      </div>
+      <div className="">
+        <Link href="/">
+          <span className="text-xl font-semibold">Socialfolio</span>
+        </Link>
+      </div>
+
+      
+      <div className="flex items-center">
+        <div className="mr-3">
+          <ThemeSwitcher />
+        </div>
+      </div>
+    </nav>
+  );
+}
