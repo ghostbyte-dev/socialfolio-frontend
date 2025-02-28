b46f045b05dfde8f45e75485793fae2df6096831
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-02-28 12:21:58 +0100

    Work on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,39 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <nav className="bg-surface-container">
-      <div className="p-4 flex justify-center">
-        <Link href="/imprint" className="mx-3 font-bold hover:underline">
-          Imprint
-        </Link>
-        <Link href="/credits" className="mx-3 font-bold hover:underline">
-          Credits
-        </Link>
+      <div className="p-4 flex justify-evenly items-center">
+        <div className="flex flex-col">
+          <Link href="/imprint" className="mb-3 font-bold hover:underline">
+            Imprint
+          </Link>
+          <Link href="/credits" className=" font-bold hover:underline">
+            Credits
+          </Link>
+        </div>
+
+        <div>
+          <span className="text-xl font-bold">Stay up to date:</span>
+          <div className="mt-3 flex">
+            <Link href="https://techhub.social/@socialfolio">
+              <img
+                className="mr-3"
+                src="/icons/mastodon.svg"
+                alt="Mastodon logo"
+                height={45}
+                width={45}
+              ></img>
+            </Link>
+
+            <Link href="https://github.com/Hiebeler/socialfolio-frontend">
+              <img
+                src="/icons/mastodon.svg"
+                alt="Github logo"
+                height={45}
+                width={45}
+              ></img>
+            </Link>
+          </div>
+        </div>
       </div>
       <div className="p-4 w-full flex justify-center items-center">
         <div className="text-center">
