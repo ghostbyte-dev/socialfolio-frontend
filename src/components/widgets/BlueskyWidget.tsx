5e8f4b02912f3e9fd56d9c554e25ad7db25cdc27
daniebeler <hiebeler.daniel@gmail.com>
2025-03-01 12:51:45 +0100

    add two more variants to bluesky widget

--- src/components/widgets/BlueskyWidget.tsx
+++ src/components/widgets/BlueskyWidget.tsx
@@ -1,5 +1,6 @@
 import { BlueskyData } from "@/types/widget-types";
 import { BaseWidget } from "./BaseWidget";
+import Bluesky from "@/assets/icons/bluesky.svg";
 
 interface BlueskyWidgetProps {
   data: BlueskyData;
@@ -10,22 +11,58 @@ interface BlueskyWidgetProps {
   editWidget: () => void;
 }
 
-export function BlueskyWidget({ data, size, isOwner, variant, deleteWidget, editWidget }: BlueskyWidgetProps) {
+export function BlueskyWidget({
+  data,
+  size,
+  isOwner,
+  variant,
+  deleteWidget,
+  editWidget,
+}: BlueskyWidgetProps) {
   const onClick = () => {
     const url = "https://bsky.app/profile/" + data.handle;
     window.location.href = url;
-  }
+  };
 
   return (
-    <BaseWidget isOwner={isOwner} isClickable={true} deleteWidget={deleteWidget} editWidget={editWidget} onClick={onClick}>
+    <BaseWidget
+      isOwner={isOwner}
+      isClickable={true}
+      deleteWidget={deleteWidget}
+      editWidget={editWidget}
+      onClick={onClick}
+    >
       {variant == 1 && (
-          <div className="h-full w-full flex justify-center items-center">
-            <img
-              src="/widgeteditor/bluesky.svg"
-              alt="Pixelfed logo"
-              className="w-[50%] h-[50%] object-contain"
-            />
-          </div>
+        <div className="h-full w-full flex justify-center items-center">
+          <img
+            src="/widgeteditor/bluesky.svg"
+            alt="Bluesky logo"
+            className="w-[50%] h-[50%] object-contain"
+          />
+        </div>
+      )}
+      {variant == 2 && (
+        <div className="h-full w-full flex justify-center items-center bg-[#1185fe]">
+          <img
+            src="/widgets/bluesky/bluesky_white.svg"
+            alt="Bluesky logo"
+            className="w-[50%] h-[50%] object-contain"
+          />
+        </div>
+      )}
+      {variant == 3 && (
+        <div className="h-full w-full flex justify-center items-center">
+          <img
+            src="/widgets/bluesky/bluesky_white.svg"
+            alt="Bluesky logo"
+            className="w-[50%] h-[50%] object-contain hidden dark:block"
+          />
+          <img
+            src="/widgets/bluesky/bluesky_black.svg"
+            alt="Bluesky logo"
+            className="w-[50%] h-[50%] object-contain block dark:hidden"
+          />
+        </div>
       )}
     </BaseWidget>
   );
