ad34b8b44168b7b56a8375ed1987e565ad43a45e
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-10-29 23:28:55 +0100

    Add selection color

--- src/components/landingpage/ExploreSection.tsx
+++ src/components/landingpage/ExploreSection.tsx
@@ -3,7 +3,7 @@ import ExploreProfileButton from "./ExploreProfileButton";
 
 export default function ExploreSection() {
   return (
-    <section className="mt-20 w-full bg-primary text-on-primary py-20">
+    <section className="mt-20 w-full bg-primary text-on-primary py-20 selection:bg-surface selection:text-on-surface">
       <div className="flex flex-col items-center content-wrapper">
         <h2 className="text-center text-4xl md:text-5xl lg:text-5xl font-bold mb-3 cursive-font">
           Explore other Socialfolios
